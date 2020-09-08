209d982ed1e892e3a84968f712928e714e2a2d22
Juan Luis <juanlvelar@gmail.com>
2020-09-08 16:21:59 +0100

    Load exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creates the cache of the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    ## Initialize the inverse matrix
+  j <- NULL
+  
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  
+  get <- function()x
+  
+  setInverse <- function(inverse) j <<- inverse
+  
+  getInverse <- function() j 
+  
+  ## List of the methods
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Checks if the value is stored in the cache and recover it or save it in 
+## the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("Getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
+
